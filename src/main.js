36a394fbec7a9d832976c01d7346b6e294e006c2
Alessandro Vizin <alessandro.vizin@valtech.de>
2019-09-24 16:05:44 +0200

    First commit i18n

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import i18n from './i18n'
 
 Vue.config.productionTip = false
 
@@ -19,5 +20,6 @@ Vue.filter('toCurrency', function (value) {
 })
 
 new Vue({
-  render: h => h(App),
+  i18n,
+  render: h => h(App)
 }).$mount('#app')
